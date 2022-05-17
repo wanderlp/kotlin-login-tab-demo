f1a79233efc8e1fabafc02e0af72c2c8225f6832
Wanderson <lopez.wanderson@gmail.com>
2022-05-17 14:25:16 -0600

    Se codificó la clase LoginFragment

--- app/src/main/java/com/example/logintabdemo/LoginFragment.kt
+++ app/src/main/java/com/example/logintabdemo/LoginFragment.kt
@@ -1,4 +1,20 @@
 package com.example.logintabdemo
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class LoginFragment {
+// Here ":" symbol is indicate that LoginFragment
+// is a child class of Fragment Class
+class LoginFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.layout_login, container, false
+        )
+    }
+
 }
